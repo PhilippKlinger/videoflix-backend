ca68f92a15f1ca451d192cb35484b57de97fba48
Philipp Klinger <klinger.breuber@web.de>
2024-04-19 12:11:14 +0200

    updated customUser model

--- videoflix_api/users/admin.py
+++ videoflix_api/users/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
     fieldsets = (
         (
             'Individuelle Daten',
--- videoflix_api/users/models.py
+++ videoflix_api/users/models.py
@@ -6,3 +6,6 @@ class CustomUser(AbstractUser):
     custom_field = models.CharField(max_length=500, blank=True, null=True)
     phone = models.CharField(max_length=50, blank=True, null=True)     
     is_active = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.username

976a05392f3466b2032575e5de517436737d03ca
Philipp Klinger <klinger.breuber@web.de>
2024-06-08 15:44:04 +0200

    updated video model

--- videoflix_api/videoflix/models.py
+++ videoflix_api/videoflix/models.py
@@ -3,9 +3,28 @@ from django.db import models
 from users.models import Profile
 
 class Video(models.Model):
+    
+    GENRE_CHOICES = [
+        ('Action', 'Action'),
+        ('Comedy', 'Comedy'),
+        ('Crime', 'Crime'),
+        ('Documentary', 'Documentary'),
+        ('Mystery', 'Mystery'),
+        ('Romance', 'Romance'),
+        ('Sports', 'Sports')
+    ]
+    
+    CATEGORY_CHOICES = [
+        ('Movie', 'Movie'),
+        ('TV-Show', 'TV-Show')
+    ]
+    
+    
     uploaded_at = models.DateField(default=date.today)
     title = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICES, default=GENRE_CHOICES[0][0])
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0])
     video_file = models.FileField(upload_to='videos', blank=True, null=True)
     thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
     favorited_by = models.ManyToManyField(Profile, related_name='favorite_videos', blank=True)

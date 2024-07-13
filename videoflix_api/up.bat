d7c389e6b852542b6018ce71d3f3b8684a603cff
Philipp Klinger <klinger.breuber@web.de>
2024-07-13 11:14:57 +0200

    automatic deployment test

--- videoflix_api/up.bat
+++ videoflix_api/up.bat
@@ -2,3 +2,4 @@ git pull
 git add .
 git commit -m "%*"
 git push
+ssh philipp@34.159.223.143 "cd /home/klinger_philipp59/projects/videoflix-backend/videoflix_api && git pull && sudo supervisorctl restart all"

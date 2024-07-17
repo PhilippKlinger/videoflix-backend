1834d79ae13152f0101ac60d909955d8c7251836
Philipp Klinger <klinger.breuber@web.de>
2024-07-17 14:42:14 +0200

    updated video serializer

--- videoflix_api/videoflix/serializers.py
+++ videoflix_api/videoflix/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Video, VideoResolution
+from .models import Video, VideoResolution, Profile
 from users.serializers import ProfileSerializer
 
 class VideoResolutionSerializer(serializers.ModelSerializer):
@@ -9,8 +9,14 @@ class VideoResolutionSerializer(serializers.ModelSerializer):
 
 class VideoSerializer(serializers.ModelSerializer):
     resolutions = VideoResolutionSerializer(many=True, read_only=True)
-    favorited_by = ProfileSerializer(many=True, read_only=True)
+    favorited_by = serializers.PrimaryKeyRelatedField(queryset=Profile.objects.all(), many=True, required=False)
 
     class Meta:
         model = Video
         fields = "__all__"
+
+    def create(self, validated_data):
+        favorited_by_data = validated_data.pop('favorited_by', [])
+        video = Video.objects.create(**validated_data)
+        video.favorited_by.set(favorited_by_data)
+        return video

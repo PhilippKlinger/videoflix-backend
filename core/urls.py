f8e61ce23b6697211d049f086e343cbbab952f55
Philipp Klinger <klinger.breuber@web.de>
2025-07-25 11:23:49 +0200

    changed authentication classes for api docu

--- core/urls.py
+++ core/urls.py
@@ -11,7 +11,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework import permissions
+from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -24,7 +24,8 @@ schema_view = get_schema_view(
         description="API documentation for the Videoflix backend.",
     ),
     public=True,
-    permission_classes=[permissions.AllowAny],
+    permission_classes=[AllowAny],
+    authentication_classes=[]
 )
 
 urlpatterns = [
